bd8459b65cd5f3deb79684a31fc26028c44363fa
Agw-a <lizagwa7@gmail.com>
2022-04-19 13:46:51 -0700

    Adding task 0

--- _printf.c
+++ _printf.c
@@ -4,42 +4,40 @@
  * @format: character string
  * Return: nmber of characters printed
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	};
+	int (*func)(va_list, flags_t *);
+	const char *p;
+	va_list ap;
+	flags_t flags = {0, 0, 0};
 
-	va_list args;
-	int i = 0, j, len = 0;
+	register int cnt = 0;
 
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	va_start(ap, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		j = 13;
-		while (j >= 0)
+		if (*p == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			p++;
+			if (*p == '%')
 			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
+				cnt += _putchar('%');
+				continue;
 			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
+			while (get_flag(*p, &flags))
+				p++;
+			func = get_print(*p);
+			cnt += (func)
+				? func(ap, &flags)
+				: _printf("%%%c", *p);
+		} else
+			cnt += _putchar(*p);
 	}
-	va_end(args);
-	return (len);
+	_putchar(-1);
+	va_end(ap);
+	return (cnt);
 }
--- main.h
+++ main.h
@@ -8,40 +8,47 @@
 #include <stdlib.h>
 
 /**
- * struct format-match the conversion specifiers for printf
- * @id:type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f:type pointer to function for the conversion specifier
- *
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
  */
 
-typedef struct format
+typedef struct flags
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	int plus;
+	int space;
+	int hash;
+} flags_t;
 
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/*_printf*/
 int _printf(const char *format, ...);
-int printf_pointer(va_list val);
-int printf_hex_aux(unsigned long int num);
-int printf_HEX_aux(unsigned int num);
-int printf_exclusive_string(va_list val);
-int printf_HEX(va_list val);
-int printf_hex(va_list val);
-int printf_oct(va_list val);
-int printf_unsigned(va_list args);
-int printf_bin(va_list val);
-int printf_srev(va_list args);
-int printf_rot13(va_list args);
-int printf_int(va_list args);
-int printf_dec(va_list args);
-int _strlen(char *s);
-int *_strcpy(char *dest, char *src);
-int _strlenc(const char *s);
-int rev_string(char *s);
-int _strlenc(const char *s);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
+
+/*print number*/
+int print_int(va_list l, flags_t *f);
+void print_number(int n);
+int print_unsigned(va_list l, flags_t *f);
+int cnt_digit(int i);
+
+/*get_print*/
+int (*get_print(char s))(va_list, flags_t *);
+
+/*get_flags*/
+int get_flag(char s, flags_t *f);
+
+
+
+/*write_funcs*/
 int _putchar(char c);
+int _puts(char *str);
+
+
 
 #endif
