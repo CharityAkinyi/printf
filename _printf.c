ceacae2fd9683ea820a059eeae1d64b615e34877
Agw-a <lizagwa7@gmail.com>
2022-04-19 13:10:48 -0700

    Adding a function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -4,32 +4,42 @@
  * @format: character string
  * Return: nmber of characters printed
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
+		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
+		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
+		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	};
 
-	int i;
-	int len = 0;
-	char *newstr = NULL;
+	va_list args;
+	int i = 0, j, len = 0;
 
-	while (format[len] != '\0')
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
 	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
 		len++;
+		i++;
 	}
-
-	newstr = malloc(sizeof(char) * (len + 1));
-
-	if (newstr == NULL)
-	{
-		return (1);
-	}
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		newstr[i] = format[i];
-	}
-	newstr[i] = '\0';
-
-	write(1, format, len);
-	free(newstr);
-	return (0);
+	va_end(args);
+	return (len);
 }
