dfa989e2e02f14aa2b2b104ff015931917b1ad9a
Agw-a <lizagwa7@gmail.com>
2022-04-22 01:09:50 -0700

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,43 +1,84 @@
 #include "main.h"
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
 /**
- * _printf - produces output according to format
- * @format: character string
- * Return: nmber of characters printed
- */
+* cleanup - Peforms cleanup operations for _printf.
+* @args: A va_list of arguments provided to _printf.
+* @output: A buffer_t struct.
+*/
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+* run_printf - Reads through the format string for _printf.
+* @format: Character string to print - may contain directives.
+* @output: A buffer_t struct containing a buffer.
+* @args: A va_list of arguments.
+*
+* Return: The number of characters stored to output.
+*/
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+	unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+	len = 0;
+	if (*(format + i) == '%')
+	{
+	tmp = 0;
+	flags = handle_flags(format + i + 1, &tmp);
+	wid = handle_width(args, format + i + tmp + 1, &tmp);
+	prec = handle_precision(args, format + i + tmp + 1,&tmp);
+	len = handle_length(format + i + tmp + 1, &tmp);
+	f = handle_specifiers(format + i + tmp + 1);
+	if (f != NULL)
+	{
+	i += tmp + 1;
+	ret += f(args, output, flags, wid, prec, len);
+	continue;
+	}
+	else if (*(format + i + tmp + 1) == '\0')
+	{
+	ret = -1;
+	break;
+	}
+	}
+	ret += _memcpy(output, (format + i), 1);
+	i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+* _printf - Outputs a formatted string.
+* @format: Character string to print - may contain directives.
+*
+* Return: The number of characters printed.
+*/
 int _printf(const char *format, ...)
 {
-	int (*func)(va_list, flags_t *);
-	const char *p;
-	va_list ap;
-	flags_t flags = {0, 0, 0};
-
-	register int cnt = 0;
-
-	va_start(ap, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				cnt += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			func = get_print(*p);
-			cnt += (func)
-				? func(ap, &flags)
-				: _printf("%%%c", *p);
-		} else
-			cnt += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(ap);
-	return (cnt);
+	buffer_t *output;
+	va_list args;
+	int ret;
+	if (format == NULL)
+	return (-1);
+	output = init_buffer();
+	if (output == NULL)
+	return (-1);
+	va_start(args, format);
+	ret = run_printf(format, args, output);
+	return (ret);
 }
